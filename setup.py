d6270f75946fa5593ee534a38f7a05f585e13b34
arun <aj@accubits.com>
2020-02-27 15:02:19 +0530

    README.md

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
 from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='forex-converter',
     packages=['forex-converter'],
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='A simple python package for currency conversion',
     author='Jithin VG',
-    author_email='jithinvg@accubits.com, aj@accubits.com',  # Type in your E-Mail
-    url='https://github.com/accubits/currency-converter-python.git',  # Provide either the link to your github or to your website
-    download_url='https://github.com/accubits/currency-converter-python/archive/0.1.tar.gz',  # I explain this later on
-    keywords=['currency', 'forex-converter', 'currency converter', ' converter currency','forex'],  # Keywords that define your package best
+    author_email='jithinvg@accubits.com, aj@accubits.com',
+    url='https://github.com/accubits/currency-converter-python.git',
+    download_url='https://github.com/accubits/currency-converter-python/archive/0.2.tar.gz',
+    keywords=['currency', 'forex-converter', 'currency converter', ' converter currency', 'forex'],
     install_requires=[
         'requests'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Currency Conversion',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
 )
